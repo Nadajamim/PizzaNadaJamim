3ac09a643b0858a45548e76fd705dfeccd2d4dcf
Houssam Byoud <byoudhoussam@gmail.com>
2025-03-16 04:15:02 +0000

    Initial commit

---
+++ app/src/main/java/com/example/MyPizzaAPP/PizzaDetailActivity.java
@@ -0,0 +1,55 @@
+package com.example.MyPizzaAPP;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.MyPizzaAPP.Beans.Pizza;
+import com.example.MyPizzaAPP.service.ProduitService;
+import com.example.MyPizzaAPP.R;
+
+public class PizzaDetailActivity extends AppCompatActivity {
+
+    private ImageView pizzaImage;
+    private TextView pizzaName, descriptionTitle, descriptionText;
+    private TextView ingredientsTitle, ingredientsText;
+    private ProduitService pizzaService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizza_detail);
+
+        pizzaImage = findViewById(R.id.pizzaDetailImage);
+        pizzaName = findViewById(R.id.pizzaDetailName);
+        descriptionTitle = findViewById(R.id.descriptionTitle);
+        descriptionText = findViewById(R.id.descriptionText);
+        ingredientsTitle = findViewById(R.id.ingredientsTitle);
+        ingredientsText = findViewById(R.id.ingredientsText);
+
+
+        int pizzaId = getIntent().getIntExtra("PIZZA_ID", -1);
+
+        pizzaService = ProduitService.getInstance();
+
+        if (pizzaId != -1) {
+            Pizza pizza = pizzaService.findById(pizzaId);
+            if (pizza != null) {
+                displayPizzaDetails(pizza);
+            }
+        }
+
+    }
+
+    private void displayPizzaDetails(Pizza pizza) {
+        pizzaImage.setImageResource(pizza.getPhoto());
+        pizzaName.setText(pizza.getNom());
+        descriptionTitle.setText("Description :");
+        descriptionText.setText(pizza.getDescription());
+        ingredientsTitle.setText("Ingredients :");
+        ingredientsText.setText(pizza.getDetailsIngred());
+    }
+}
